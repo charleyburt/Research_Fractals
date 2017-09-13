faca2f1061ae7891f95093ab9598cdc0578a47de
cmb11 <charley.burtwistle@doane.edu>
2017-09-13 12:25:24 -0500

    saves radius, still need theta

--- Polar_Converter.py
+++ Polar_Converter.py
@@ -3,17 +3,18 @@
 #Last Updated: September 13, 2017
 
 import csv
+import numpy as np
 
 def cartesian_to_polar():
 
     input_file = input("Enter file path: ")
-
-    with open(input_file) as cartesian_file:
-        reader = csv.reader(cartesian_file)
-        for count, (c1, c2) in enumerate(reader):
-            with open ('results/polar_results' + "triangle_polar" + "_iterations_.csv", 'w') as polar_file:
+    input_iterations = input("How many iterations: ")
+    with open ('results/polar_results/POLAR_' + input_iterations + "_iterations_.csv", 'w') as polar_file:
+        with open(input_file) as cartesian_file:
+            reader = csv.reader(cartesian_file)
+            for count, (c1, c2) in enumerate(reader):
                 theta = c1
-                r = c2
+                r = np.sqrt((c1**2)+(c2**2))
                 polar_file.write('{},{} \n'.format(theta , r))
 
 
