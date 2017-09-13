456cb58fa4b437db75f5c2c511532f034599b350
cmb11 <charley.burtwistle@doane.edu>
2017-09-13 14:16:36 -0500

    DEV polar converter is pretty much working

--- Polar_Converter.py
+++ Polar_Converter.py
@@ -12,22 +12,24 @@ def cartesian_to_polar():
     with open ('results/polar_results/POLAR_' + input_iterations + "_iterations_.csv", 'w') as polar_file:
         with open(input_file) as cartesian_file:
             reader = csv.reader(cartesian_file)
-            for count, (c1, c2) in enumerate(reader):
-                if(int(c1) > 0):
-                    theta = np.arctan(c2/c1)
-                if(c1<0 and c2>=0):
-                    theta = np.arctan(c2/c1) + np.pi
-                if(c1<0 and c2<0):
-                    theta = np.arctan(c2/c1) - np.pi
-                if(c1==0 and c2>0):
+            for count, (x_string, y_string) in enumerate(reader):
+                x_float = round(float(x_string),5)
+                y_float = round(float(y_string),5)
+                if(x_float > 0):
+                    theta = np.arctan(y_float/x_float)
+                if(x_float<0 and y_float>=0):
+                    theta = np.arctan(y_float/x_float) + np.pi
+                if(x_float<0 and y_float<0):
+                    theta = np.arctan(y_float/x_float) - np.pi
+                if(x_float==0 and y_float>0):
                     theta = np.pi/2
-                if(c1==0 and c2<0):
-                    theta = -np.pi/2
-                if(c1==0 and c2==0):
+                if(x_float==0 and y_float<0):
+                    theta = (3*np.pi)/2
+                if(x_float==0 and y_float==0):
                     theta = "undefined"
 
                 degrees = (theta*180)/np.pi
-                r = np.sqrt((c1**2)+(c2**2))
+                r = np.sqrt((x_float**2)+(y_float**2))
 
                 polar_file.write('{},{} \n'.format(degrees , r))
 
