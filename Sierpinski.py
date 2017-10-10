8ade1aaa1fa4ebf11b0e5880712d1399342eae38
cmb11 <charley.burtwistle@doane.edu>
2017-10-10 10:43:55 -0500

    Changes Sierpinski.py to reflect Dr. Williams suggestions (3x faster, less recorded points

--- Sierpinski.py
+++ Sierpinski.py
@@ -1,75 +1,38 @@
-#Title: Koch Snowflake
-#Author: Charley Burtwistle
-#Last Updated: September 11, 2017
-
-
-#import `turtle` to draw the fractal
 from turtle import *
-import time
-import numpy as np
 
 
 
-distance = 0 #create distance variable to track circumference
+def Side(l,d):
+  if(d==0):
+     forward(l)
+     return
 
-#Draws the koch curve (i.e. "one side" of the koch snowflake)
-def koch_curve(side_length, iterations):   #pass in the side length and how many iterations
-
-
-    if iterations == 0:    # if 0 iterations
-        forward(side_length)    # draw one side
-        global distance # make distance global so it can be printed out
-        distance += side_length #add the side length to distance
-
-        return
-
-    #draw koch curve (this will be repeated for each iteration)
-
-    #reduce side length by 1/3
-    side_length /= 2.0
-
-    #draw "over"
-    koch_curve(side_length, iterations-1)
-
-    #draw "up"
-    left(120)
-    koch_curve(side_length, iterations-1)
-
-    #draw "down"
-    right(120)
-    koch_curve(side_length, iterations-1)
-
-    #draw "over"
-    right(120)
-    koch_curve(side_length, iterations-1)
-
-    left(120)
-    koch_curve(side_length, iterations-1)
-
-#Draws the koch curve 3 times (i.e. the koch snowflake)
-def Snowflake(side_length, iterations):    #pass in the side length and number of iterations
-
-    for i in range(3): #for each side of the triangle
-        koch_curve(side_length, iterations) #draw the koch curve
-        right(120) #move on to the next side
-
-    print(distance) #when the entire thing is drawn, print it out
+  Side(l/2, d-1)
+  diagUp(l/2)
+  Side(l/2,d-1)
+  diagDown(l/2)
+  Side(l/2,d-1)
 
 
+def diagUp(l):
+
+   left(120)
+   forward(l)
+   right(120)
 
 
+def diagDown(l):
+   right(120)
+   forward(l)
+   left(120)
 
-
-# Code to run from command line
 if __name__ == "__main__":
-    speed(10)
-    length = 300
-    penup()
-    backward(length/2.0)
-    left(90)
-    forward(np.sqrt((length**2)-((length/2.0)**2))/3)
-    right(90)
-    pendown()
-    input_iterations = int(input("How many iterations? : "))
-    Snowflake(length, input_iterations)
-    done()
+      speed('fastest')
+      depth = 5
+      size  = 300
+      Side(size,depth)
+      right(120)
+      Side(size,depth)
+      right(120)
+      Side(size,depth)
+      done()
